279d7cf1fa4106767cea9f2d2ec1bd79eb1f7b1d
turui <991642404@qq.com>
2021-10-25 23:48:02 +0800

    1

---
+++ led.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+while True:
+    GPIO.output(11, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(11, GPIO.LOW)
+    time.sleep(1)
+
+GPIO.cleanup()
+
+
